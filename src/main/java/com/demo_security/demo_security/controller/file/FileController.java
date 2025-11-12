3142e6f99f9b3fb60d4bddb071365902ca63ef96
dev4-gmail2025 <sevago.dev4@gmail.com>
2025-11-12 16:22:07 +0700

    Add FileController for file management endpoints with upload, retrieval, and deletion functionalities

--- src/main/java/com/demo_security/demo_security/controller/file/FileController.java
+++ src/main/java/com/demo_security/demo_security/controller/file/FileController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 
 @RestController
+@Tag(name = "File", description = "File management endpoints")
 @RequestMapping("/api/files")
 public class FileController {
     @Autowired
